81f5ac5f822169e3914c4eaa631118b86fc9742a
quyenpro123 <101342563+quyenpro123@users.noreply.github.com>
2022-10-04 13:58:38 +0700

    thêm comment

--- src/main/java/quyen/demo1/Main.java
+++ src/main/java/quyen/demo1/Main.java
@@ -1,7 +1,6 @@
 package quyen.demo1;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -17,9 +16,12 @@ public class Main extends Application {
   public void start(Stage stage) {
     stage.setTitle("My APP");
     Button button = new Button("Hello");
-    StackPane layout = new StackPane(button);
+    Button button1 = new Button("OK");
+    StackPane layout = new StackPane(button, button1);
     Scene scene = new Scene(layout, 800.0, 200.0);
     stage.setScene(scene);
     stage.show();
+    /**oke nha cac ben*/
+
   }
 }
